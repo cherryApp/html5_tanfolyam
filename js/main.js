2b71895a208cb3b3d90bd5dfade4c36e86fda1e7
unknown <html@2-00.itfactory.neta>
2015-09-28 16:01:29 +0200

    Első nap teljes anyaga.

--- js/main.js
+++ js/main.js
@@ -1,2 +1,13 @@
 // alert( "Main.js" );
 $( ".bs-docs-popover" ).popover();
+
+var foValtozo = 3;
+
+function testFunc() {
+    console.log( "foValtozo: ", foValtozo );
+    var masik = 4;
+}
+
+function testFunc2() {
+    console.log( "masik: ", masik );    
+}
